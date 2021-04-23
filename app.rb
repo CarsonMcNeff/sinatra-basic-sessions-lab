573f59a35a7c51a41b2577f467a18d96cbe2e381
CarsonMcNeff <cjmcneff@outlook.com>
2021-04-23 17:31:24 -0500

    commit tree edc55ea2c7c246e732932e76415757c01cc8bbb3

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions 
+        set :session_secret, "026d0456e249c879299ef2e01b31341d2830079b762e7ba07ddf0a4367079ba8383aac3602f04c4d8a1ec588ad884bd8980d7e553b34112bb9c898e056411f22"
+    end
+
+    get '/' do 
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session 
+        erb :checkout
+    end
+
 end
